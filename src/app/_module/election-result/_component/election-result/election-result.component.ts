ec10e69e046bc602c3e902656da8ce0acc14d5c0
nirmalisrael <nirmal.israel126@gmail.com>
2024-01-29 16:02:54 +0530

    create result module in lab pc

--- src/app/_module/election-result/_component/election-result/election-result.component.ts
+++ src/app/_module/election-result/_component/election-result/election-result.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Election } from 'src/app/_module/election/_dto/election';
+import { ElectionStatus } from 'src/app/_module/election/_dto/election-status';
 
 @Component({
   selector: 'app-election-result',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class ElectionResultComponent {
 
+  statusOptions = Object.values(ElectionStatus);
+
+  elections: Election[] = [
+    {electionName: 'Fine Arts Secretary', electionDate: new Date('2024-01-20'), electionStatus: ElectionStatus.LIVE},
+    {electionName: 'Sports Secretary', electionDate: new Date('2024-01-20'), electionStatus: ElectionStatus.COMPLETED},
+    {electionName: 'Chair Man', electionDate: new Date('2024-01-20'), electionStatus: ElectionStatus.UPCOMMING},
+    {electionName: 'Media Secretary', electionDate: new Date('2024-01-20'), electionStatus: ElectionStatus.LIVE}
+  ];
+
+  getWinnerByElectionName(electionName: string | undefined): string {
+    return 'LCVCM01';
+  }
 }
