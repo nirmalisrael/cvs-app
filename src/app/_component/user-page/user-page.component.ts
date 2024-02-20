0f61b74896457aebf70067e4bff3a2cb21ab675d
nirmalisrael <nirmal.israel126@gmail.com>
2024-02-20 13:34:05 +0530

    finish the vote button in studen tpage

--- src/app/_component/user-page/user-page.component.ts
+++ src/app/_component/user-page/user-page.component.ts
@@ -10,11 +10,24 @@ export class UserPageComponent {
 
   elctionNames: string[] = ["Election 1", "Election 1", "Election 1"];
 
-  showSelectElection = false;
+  showSelectElection = true;
 
-  showVotingPage = true;
+  showVotingPage = false;
+
+  selectedCandidateId?: string;
 
   candidate: Candidate = {
     candidateId: "LCVSS01", candidateName: "Nirmal", candidateDeptNo: "21UCS14"
+  }
+
+  candidates: Candidate[] = [
+    {candidateId: 'LCVFA01', candidateDeptNo: '21UCS20', candidateName: 'Vetri Piriyan'},
+    {candidateId: 'LCVFA02', candidateDeptNo: '21UCS21', candidateName: 'Nirmal'},
+    {candidateId: 'LCVFA03', candidateDeptNo: '21UCS33', candidateName: 'Santhosh'},
+    {candidateId: 'LCVFA04', candidateDeptNo: '21UCS45', candidateName: 'Maria Raj'},
+  ];
+
+  onSelectCandidateId(candidateId: string | undefined) {
+    console.log(this.selectedCandidateId);
   }
 }
